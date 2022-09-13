c79747fc5b389250fb9c0fe7bbe000fa93eb74f9
Mohammad Mustafijur Rahman <rahmanbond0@gmail.com>
2022-09-13 19:34:38 +0800

    first commit

---
+++ ifElse.js
@@ -0,0 +1,22 @@
+
+// how much kind of there got A+
+
+
+var marks = 80;
+
+if (marks >= 80) {
+    console.log('A');
+}
+
+else if (marks >= 60) {
+    console.log('B');
+}
+else if (marks >= 50) {
+    console.log('C');
+}
+else if (marks >= 40) {
+    console.log('D');
+}
+else {
+    console.log('F');
+}
